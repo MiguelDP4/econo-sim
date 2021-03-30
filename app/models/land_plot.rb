3d26347c20d62af46ed9f5de12125c529c89fb2e
migueldp4 <migueldp4@gmail.com>
2021-03-30 15:19:25 -0600

    associate user and land plot

--- app/models/land_plot.rb
+++ app/models/land_plot.rb
@@ -1,2 +1,5 @@
 class LandPlot < ApplicationRecord
+  belongs_to :user
+
+  def get_stats()
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_11_173728) do
+ActiveRecord::Schema.define(version: 2021_03_30_182653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "land_plots", force: :cascade do |t|
+    t.integer "position_x"
+    t.integer "position_y"
+    t.integer "fertility"
+    t.integer "trees"
+    t.integer "stone"
+    t.integer "iron_ore"
+    t.integer "copper_ore"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_land_plots_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
@@ -42,4 +56,5 @@ ActiveRecord::Schema.define(version: 2021_03_11_173728) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "land_plots", "users"
 end
